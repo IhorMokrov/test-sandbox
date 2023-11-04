bdedd83c191fe46ef667fe7e10b7de229ad73af8
Ihor Mokrov <i.mokrov89@gmail.com>
2023-11-04 19:07:19 +0200

    12

--- script.js
+++
@@ -1,6 +0,0 @@
-let dropDownMenu = document.getElementById("navigation");
-let burgerButton = document.getElementById("burger-button");
-
-burgerButton.addEventListener("click", function () {
-  dropDownMenu.style.display = "block";
-});
