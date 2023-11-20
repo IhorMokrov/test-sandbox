c410c27197481b9d448e04ab0d726e4348bb8b41
Ihor Mokrov <i.mokrov89@gmail.com>
2023-11-20 15:36:34 +0200

    12

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-let burgerButton = document.getElementById('burger-button');
-let backdropMenu = document.getElementById('navigation');
+let burgerButton = document.getElementById("burger-button");
+let backdropMenu = document.getElementById("navigation");
 
-burgerButton.addEventListener('click', function () {
-
-    if (backdropMenu.isOpen !== true) {
-        backdropMenu.style.opacity = 1;
-        backdropMenu.style.pointerEvents = 'auto';
-        backdropMenu.style.transform = "translateY(100%)";
-         backdropMenu.style.zIndex = 3;
-        backdropMenu.isOpen = true;
-        
-    }
-    else {
-        backdropMenu.style.opacity = 0.2;
-        backdropMenu.style.pointerEvents = 'none';
-        backdropMenu.style.transform = "translateY(0%)";
-        backdropMenu.style.zIndex = 2;
-        backdropMenu.isOpen = false;
-    }
-})
-    
+burgerButton.addEventListener("click", function () {
+  if (backdropMenu.isOpen !== true) {
+    backdropMenu.style.opacity = 1;
+    backdropMenu.style.pointerEvents = "auto";
+    backdropMenu.style.transform = "translateY(100%)";
+    backdropMenu.isOpen = true;
+  } else {
+    backdropMenu.style.opacity = 0.2;
+    backdropMenu.style.pointerEvents = "none";
+    backdropMenu.style.transform = "translateY(0%)";
+    backdropMenu.isOpen = false;
+  }
+});
