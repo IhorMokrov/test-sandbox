f3b79853e26045918e72cb6ecf2fdb39e95a69aa
Ihor Mokrov <60344728+IhorMokrov@users.noreply.github.com>
2023-11-20 07:25:54 +0200

    12

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ let backdropMenu = document.getElementById('navigation');
 
 burgerButton.onclick = function () {
 
+    if (backdropMenu.status !== 'open') {
     backdropMenu.style.opacity = 1;
-    backdropMenu.style.pointerEvents = 'auto';  
-    backdropMenu.style.top = '100%';
+        backdropMenu.style.pointerEvents = 'auto';  
+        backdropMenu.style.transform = "translateY(100%)";
+        backdropMenu.status = 'open';
+        
+    } 
+    else {
+         backdropMenu.style.opacity = 0.2;
+        backdropMenu.style.pointerEvents = 'none';  
+           backdropMenu.style.transform = "translateY(0%)";
+    backdropMenu.status = 'closed';
+    }
    
     
 }
